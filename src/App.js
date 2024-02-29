edaed6e3ad584d195f0577065da2b479362174b2
JoshFowles <fowlesjoshuam@gmail.com>
2024-02-29 14:37:01 -0700

    Add parent and additional flexbox
    added a parent column flexbox so additional divs will position vertically and page can be more flexibly configured

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import './App.css';
+import './assets/Flasher Gremlin.gif';
 
 function App() {
   return (
-    
 
-//setup background gif and header
-<div style={{
-      backgroundImage: 'url(https://imgur.com/pYk8ItK.gif)',
-      backgroundSize: 'cover',  
-      backgroundRepeat: 'no-repeat',
-      height: '600px', 
-      display: 'flex',
-      position: 'relative',
-      backgroundPosition: 'center',
-      textAlign: 'center'
-    }}>
 
-    <h1 align = 'center' style={{ fontSize: '40px', color:'rgb(255, 253, 208)'}}>
-      Gremlins Emporium 
-      <div>
+    //setup background gif and header
+    <div className='Mohawk-header'>
+      <div style={{
+        backgroundImage: 'url(https://imgur.com/pYk8ItK.gif)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '600px',
+        backgroundPosition: 'center',
+        //textAlign: 'center'
+      }}>
 
-      <a href="https://www.gremlns.com" style={{fontSize: '15px', color: "rgb(255, 253, 208)"}}>
-      Additional Material Here
-      </a>
+
+        <div style={{ margin: '0px 20px' }}>
+          <h1 style={{ fontSize: '40px', color: '#FFFDD0' }}>
+            Gremlins Emporium
+          </h1>
+
+          <a href="https://www.gremlns.com" style={{ fontSize: '15px', color: '#FFFDD0' }}>
+            Additional Material Here
+          </a>
+
+          <div>
+            <p style={{ color: '#FFFDD0' }}>
+              This page will contain a lot of Gremlins
+            </p>
+          </div>
+        </div>
 
       </div>
 
-      </h1>
-
-      <div>
-      <p style={{color: 'rgb(255, 253, 208)'}}>
-        This page will contain a lot of Gremlins
-      </p>
+      <div className="Gremlins">
+        <div>
+          <p>This is one item</p>
+          <img src={require('./assets/gizmoHides.gif')} alt='scaredGizmo' />
         </div>
+        <div>
+          <p>This is the second item</p>
+          <img src={require('./assets/Flasher Gremlin.gif')} alt='Flasher' />
+        </div>
+        <div>
+          <p>This is the third item</p>
+          <img src={require('./assets/gizmoAwe.gif')} alt='amazedGizmo' />
+        </div>
+      </div>
+
 
     </div>
-   
+
 
   );
 }
