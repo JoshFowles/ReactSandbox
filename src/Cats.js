7a4d4cafb12bf8d9a8d462914e0370db00814d55
JoshFowles <fowlesjoshuam@gmail.com>
2024-03-04 16:13:26 -0700

    Add header and paragraph to Cats.js
    added rainbow header and a paragraph with a heart image span

--- src/Cats.js
+++ src/Cats.js
@@ -3,53 +3,18 @@ import './App.css';
 function Cats() {
   return (
 
+    <div>
 
-    //setup background gif and header
-    <div className='Mohawk-header'>
-      <div style={{
-        backgroundImage: 'url(https://imgur.com/pYk8ItK.gif)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        height: '600px',
-        backgroundPosition: 'center',
-        //textAlign: 'center'
-      }}>
-
-
-        <div style={{ margin: '0px 20px' }}>
-          <h1 style={{ fontSize: '40px', color: '#FFFDD0' }}>
-            Gremlins Emporium
-          </h1>
-
-          <a href="https://www.gremlns.com" style={{ fontSize: '15px', color: '#FFFDD0' }}>
-            Additional Material Here
-          </a>
-
-          <div>
-            <p style={{ color: '#FFFDD0' }}>
-              This page will contain a lot of Gremlins
-            </p>
-          </div>
-        </div>
-
+      <div className='Cat-header'>
+        <h1>Bekah's Burmese</h1>
       </div>
-
-      <div className="Gremlins">
-        <div>
-          <p>This is one item</p>
-          <img src={require('./assets/gizmoHides.gif')} alt='scaredGizmo' />
-        </div>
-        <div>
-          <p>This is the second item</p>
-          <img src={require('./assets/Flasher Gremlin.gif')} alt='Flasher' />
-        </div>
-        <div>
-          <p>This is the third item</p>
-          <img src={require('./assets/gizmoAwe.gif')} alt='amazedGizmo' />
-        </div>
+      <div className='Breed-paragraph'>
+        <p> Bringing you the best &nbsp;
+          <span>
+            <img style= {{width: '40px', height: '40px', verticalAlign: 'middle'}} src= {require('./assets/pinkHeart.jpg')} alt='heart' /></span> 
+            &nbsp; Burmese kittens around!
+          </p>
       </div>
-
-
     </div>
 
 
