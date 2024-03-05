9036f36cfa81fa8764474036229b10434b1714fc
JoshFowles <fowlesjoshuam@gmail.com>
2024-03-04 18:14:04 -0700

    Add gallery and images to fit gallery.
    Added images and a gallery that would overflow and flow horizontally. Also added a small cat drawing that shows in front of the gallery and is positioned absolutely at the bottom right portion of the gallery.

--- src/Cats.js
+++ src/Cats.js
@@ -15,6 +15,21 @@ function Cats() {
             &nbsp; Burmese kittens around!
           </p>
       </div>
+
+      <div className='Gallery'>
+        <img className='image' src= {require('./assets/kitten.webp')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/cozyCat.webp')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/cuteCat.jpg')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/naughtyKitty.jpg')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/twoKitty.jpg')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/catInDistress.jpeg')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/wiseKitty.png')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/sphinxCat.jpg')} alt='cozyCat' />
+        <img className='image' src= {require('./assets/weirdSleepCat.webp')} alt='cozyCat' />
+        
+        <img className='logo' src= {require('./assets/logoCat.webp')} alt='catDrawn' />
+
+      </div>
     </div>
 
 
